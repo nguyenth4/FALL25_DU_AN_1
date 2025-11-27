a6840987205349f4484262a023a2531a19871ac3
HuynhTran-KhangHy <khanghy.huynhtran031006@gmail.com>
2025-11-27 13:09:14 +0700

    [HTML][Client] - contact - Giao diện nội dung

--- DU_AN_1/components/contact.php
+++ DU_AN_1/components/contact.php
@@ -5,60 +5,52 @@
             <div class="row g-4">
                 <div class="col-12">
                     <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 900px;">
-                        <h4 class="text-primary border-bottom border-primary border-2 d-inline-block pb-2">Get in
-                            touch</h4>
-                        <p class="mb-5 fs-5 text-dark">We are here for you! how can we help, We are here for you!
-                        </p>
+                        <h4 class="text-primary border-bottom border-primary border-2 d-inline-block pb-2">Liên hệ</h4>
+                        <p class="mb-5 fs-5 text-dark">Chúng tôi luôn ở đây để hỗ trợ bạn! Chúng tôi có thể giúp gì cho bạn?</p>
                     </div>
                 </div>
                 <div class="col-lg-7">
-                    <h5 class="text-primary wow fadeInUp" data-wow-delay="0.1s">Let’s Connect</h5>
-                    <h1 class="display-5 mb-4 wow fadeInUp" data-wow-delay="0.3s">Send Your Message</h1>
-                    <p class="mb-4 wow fadeInUp" data-wow-delay="0.5s">The contact form is currently inactive. Get a
-                        functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste
-                        the files, add a little code and you're done. <a
-                            href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
+                    <h5 class="text-primary wow fadeInUp" data-wow-delay="0.1s">Kết nối với chúng tôi</h5>
+                    <h1 class="display-5 mb-4 wow fadeInUp" data-wow-delay="0.3s">Gửi Tin Nhắn Của Bạn</h1>
+                    <p class="mb-4 wow fadeInUp" data-wow-delay="0.5s">
+                        "Cần tư vấn kính hay đo mắt? Lumos đã có biểu mẫu Ajax & PHP sẵn sàng, chỉ việc sao chép và dùng."
+                        <a href="">Xem ngay</a>.
+                    </p>
                     <form>
                         <div class="row g-4 wow fadeInUp" data-wow-delay="0.1s">
                             <div class="col-lg-12 col-xl-6">
                                 <div class="form-floating">
                                     <input type="text" class="form-control" id="name" placeholder="Your Name">
-                                    <label for="name">Your Name</label>
+                                    <label for="name">Tên của bạn</label>
                                 </div>
                             </div>
                             <div class="col-lg-12 col-xl-6">
                                 <div class="form-floating">
                                     <input type="email" class="form-control" id="email" placeholder="Your Email">
-                                    <label for="email">Your Email</label>
+                                    <label for="email">Email của bạn</label>
                                 </div>
                             </div>
                             <div class="col-lg-12 col-xl-6">
                                 <div class="form-floating">
                                     <input type="phone" class="form-control" id="phone" placeholder="Phone">
-                                    <label for="phone">Your Phone</label>
-                                </div>
-                            </div>
-                            <div class="col-lg-12 col-xl-6">
-                                <div class="form-floating">
-                                    <input type="text" class="form-control" id="project" placeholder="Project">
-                                    <label for="project">Your Project</label>
+                                    <label for="phone">Số điện thoại</label>
                                 </div>
                             </div>
                             <div class="col-12">
                                 <div class="form-floating">
-                                    <input type="text" class="form-control" id="subject" placeholder="Subject">
-                                    <label for="subject">Subject</label>
+                                    <input type="text" class="form-control" id="subject" placeholder="Nội dung yêu cầu">
+                                    <label for="subject">Nội dung yêu cầu</label>
                                 </div>
                             </div>
                             <div class="col-12">
                                 <div class="form-floating">
                                     <textarea class="form-control" placeholder="Leave a message here" id="message"
                                         style="height: 160px"></textarea>
-                                    <label for="message">Message</label>
+                                    <label for="message">Tin nhắn</label>
                                 </div>
                             </div>
                             <div class="col-12">
-                                <button class="btn btn-primary w-100 py-3">Send Message</button>
+                                <button class="btn btn-primary w-100 py-3">Gửi Tin Nhắn</button>
                             </div>
                         </div>
                     </form>
@@ -66,7 +58,7 @@
                 <div class="col-lg-5 wow fadeInUp" data-wow-delay="0.2s">
                     <div class="h-100 rounded">
                         <iframe class="rounded w-100" style="height: 100%;"
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387191.33750346623!2d-73.97968099999999!3d40.6974881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1694259649153!5m2!1sen!2sbd"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.840797353926!2d105.767648!3d10.045162!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a0881d5e87e64d%3A0x7b1ef0ff18423f03!2sNinh%20Ki%E1%BB%BFu%2C%20Can%20Tho%2C%20Vietnam!5e0!3m2!1sen!2sbd!4v1694259649153!5m2!1sen!2sbd"
                             loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
@@ -79,8 +71,8 @@
                                     <i class="fas fa-map-marker-alt fa-2x text-primary"></i>
                                 </div>
                                 <div>
-                                    <h4>Address</h4>
-                                    <p class="mb-2">123 Street New York.USA</p>
+                                    <h4>Địa chỉ</h4>
+                                    <p class="mb-2">123 Đường ABC, Quận Ninh Kiều, TP. Cần Thơ, Việt Nam</p>
                                 </div>
                             </div>
                         </div>
@@ -91,7 +83,7 @@
                                     <i class="fas fa-envelope fa-2x text-primary"></i>
                                 </div>
                                 <div>
-                                    <h4>Mail Us</h4>
+                                    <h4>Email</h4>
                                     <p class="mb-2">info@example.com</p>
                                 </div>
                             </div>
@@ -103,7 +95,7 @@
                                     <i class="fa fa-phone-alt fa-2x text-primary"></i>
                                 </div>
                                 <div>
-                                    <h4>Telephone</h4>
+                                    <h4>Điện thoại</h4>
                                     <p class="mb-2">(+012) 3456 7890</p>
                                 </div>
                             </div>
@@ -115,8 +107,8 @@
                                     <i class="fab fa-firefox-browser fa-2x text-primary"></i>
                                 </div>
                                 <div>
-                                    <h4>Yoursite@ex.com</h4>
-                                    <p class="mb-2">(+012) 3456 7890</p>
+                                    <h4>Website</h4>
+                                    <p class="mb-2">Yoursite@ex.com</p>
                                 </div>
                             </div>
                         </div>
