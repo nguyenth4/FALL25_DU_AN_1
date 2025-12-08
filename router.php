aa2006a1aa071102ca0d44fd289c7307a7ff25fe
Lil-Tien <dominht943@gmail.com>
2025-12-08 23:20:51 +0700

    [Model] - Giỏ hàng và hủy diệt file sql cũ

--- router.php
+++ router.php
@@ -2,14 +2,21 @@
 
 require_once 'Models/Product.php';
 $productModels = new Product($connection);
-$productList = $productModels->getAllProducts(1, 10, '', 1, 'desc');
-var_dump($productList);
-$product = $productModels->getOneProduct(2);
-echo "<pre>";
-var_dump($product);
-echo "</pre>";
+// $productList = $productModels->getAllProducts(1, 10, '', 1, 'desc');
+// var_dump($productList);
+// $product = $productModels->getOneProduct(2);
+// echo "<pre>";
+// var_dump($product);
+// echo "</pre>";
 
-$product = $productModels->getOneProduct(1, 1);
-echo "<pre>";
-var_dump($product);
-echo "</pre>";
+// $product = $productModels->getOneProduct(1, 1);
+// echo "<pre>";
+// var_dump($product);
+// echo "</pre>";
+
+require_once 'Models/Cart.php';
+$cartModels = new Cart($connection);
+// $cartList = $cartModels->getAllCarts(15, 15, 1);
+// echo "<pre>";
+// var_dump($cartList);
+// echo "</pre>";
