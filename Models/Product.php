63d9fc1615b1955d0e98c180a45848a165055802
Lil-Tien <dominht943@gmail.com>
2025-12-08 22:35:18 +0700

    [Model] - Product - Phương thức lấy danh sách sản phẩm và chi tiết sản phẩm

--- Models/Product.php
+++ Models/Product.php
@@ -5,12 +5,66 @@ class Product {
     public function __construct($connection) {
         $this->connection = $connection;
     }
+    
+    /**
+     * Hàm lấy danh sách sản phẩm với phân trang và tìm kiếm
+     * @param int $page Trang hiện tại
+     * @param int $limit Số sản phẩm trên mỗi trang
+     * @param string $keyword Từ khóa tìm kiếm
+     * @param int|null $active Trạng thái kích hoạt của sản phẩm
+     * @param string $sortDate Sắp xếp theo ngày 'asc' hoặc 'desc'
+     * @return array Danh sách sản phẩm
+     */
 
-    public function getAllProducts() {
-        $query = "SELECT * FROM products";
+    public function getAllProducts($page = 1, $limit = 10, $keyword = '', $active = null, $sortDate = 'desc') {
+        $offset = ($page - 1) * $limit;
+        $search = '';
+        
+        if($active !== null) {
+            $search .= " WHERE `is_active` = :active ";
+        }
+
+        if($keyword !== '') {
+            $search = "WHERE `title` LIKE %$keyword% OR `description` LIKE %$keyword% OR `category` LIKE %$keyword%";
+        } else {
+            $search = '';
+        }
+
+        if($sortDate === 'asc' || $sortDate === 'desc') {
+            $search .= " ORDER BY `created_at` $sortDate ";
+        }
+
+        $query = "SELECT * FROM `products` $search LIMIT :limit OFFSET :offset";
         $stmt = $this->connection->prepare($query); 
+        $stmt->bindValue('"active', $active, PDO::PARAM_INT);
+        $stmt->bindValue(':limit', $limit, PDO::PARAM_INT);
+        $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Hàm lấy thông tin chi tiết của một sản phẩm theo ID, Active có thể là null thì dùng client
+     * Luôn luôn truyền active vào là số 1 để không bán sản phẩm deactive
+     * @param int $id ID của sản phẩm
+     * @param int|null $active Trạng thái kích hoạt của sản phẩm
+     * @return array Thông tin sản phẩm
+     */
+
+    public function getOneProduct($id, $active = null) {
+        if($active !== null) {
+            $search = " AND `is_active` = :active ";
+        } else {
+            $search = '';
+        }
+        $query = "SELECT * FROM `products` WHERE `product_id` = :id LIMIT 1";
+        $stmt = $this->connection->prepare($query); 
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        if($active !== null) {
+            $stmt->bindValue(':active', $active, PDO::PARAM_INT);
+        }
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 }  
 
